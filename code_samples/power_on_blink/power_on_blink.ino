83864ef18f6d24cb14662b980d3cedf9c6826605
Chetan Singh <singhchetan9924@gmail.com>
2019-07-05 11:28:35 +0200

    Reuploaded

---
+++ code_samples/power_on_blink/power_on_blink.ino
@@ -0,0 +1,40 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+void loop()
+{ 
+  int del = 700;
+  for (int i = del; i >= 0; i--)
+  {
+     if (i > 400)
+    {
+       digitalWrite(13, HIGH);
+       delay(del);
+       digitalWrite(13, LOW);
+       delay(del); 
+       del = del / 1.25;
+    }
+    else
+    {
+       digitalWrite(13, HIGH);
+       delay(del);
+       digitalWrite(13, LOW);
+       delay(del); 
+       digitalWrite(13, HIGH);
+       delay(del);
+       digitalWrite(13, LOW);
+       delay(del); 
+       digitalWrite(13, HIGH);
+       delay(del);
+       digitalWrite(13, LOW);
+       delay(del); 
+       del = del / 1.25;
+    }   
+    while(del == 0)
+    {
+      digitalWrite(13, HIGH);
+    }
+  }
+}
+
